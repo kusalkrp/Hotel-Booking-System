7a2d5e727ac63a15c867d3a262ea874cdd0b19f3
Kusal KRP <kusalcoc1212@gmail.com>
2023-12-21 22:52:15 +0530

    update

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,9 +1,138 @@
-import React from 'react'
+import React from "react";
+import "./footer.css";
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/logo.png";
 
+const quick__Links = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/about",
+    display: "About",
+  },
+  {
+    path: "/tours",
+    display: "Tours",
+  },
+];
+
+const quick__Links2 = [
+  {
+    path: "/gallery",
+    display: "Gallery",
+  },
+  {
+    path: "/login",
+    display: "Login",
+  },
+  {
+    path: "/register",
+    display: "Register",
+  },
+];
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+  const year = new Date().getFullYear();
 
-export default Footer
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="3">
+            <div className="logo">
+              <img src={logo} alt="" />
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque
+              </p>
+              <div className="social__links d-flex align-items-center gap-4">
+                <span>
+                  <Link to="#">
+                    <i class="ri-youtube-fill"></i>
+                  </Link>
+                </span>
+                <span>
+                  <Link to="#">
+                    <i class="ri-github-fill"></i>
+                  </Link>
+                </span>
+                <span>
+                  <Link to="#">
+                    <i class="ri-facebook-fill"></i>
+                  </Link>
+                </span>
+                <span>
+                  <Link to="#">
+                    <i class="ri-instagram-fill"></i>
+                  </Link>
+                </span>
+              </div>
+            </div>
+          </Col>
+          <Col lg="3">
+            <h5 className="footer__link-title">Discover</h5>
+            <ListGroup className="footer__quick-links">
+              {quick__Links.map((item, index) => (
+                <ListGroupItem key={index} className="ps-0 border-0">
+                  <Link to={item.path}>{item.display}</Link>
+                </ListGroupItem>
+              ))}
+            </ListGroup>
+          </Col>
+          <Col lg="3">
+            <h5 className="footer__link-title">Quick Links</h5>
+            <ListGroup className="footer__quick-links">
+              {quick__Links2.map((item, index) => (
+                <ListGroupItem key={index} className="ps-0 border-0">
+                  <Link to={item.path}>{item.display}</Link>
+                </ListGroupItem>
+              ))}
+            </ListGroup>
+          </Col>
+          <Col lg="3">
+            <h5 className="footer__link-title">Contact</h5>
+            <ListGroup className="footer__quick-links">
+              <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
+                <h6 className="mb-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-map-pin-line"></i>
+                  </span>
+                  Address:
+                </h6>
+                <p className="mb-0">38/5, Ariyawansha mw,Kantale, Sri Lanka</p>
+              </ListGroupItem>
+              <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
+                <h6 className="mb-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-mail-line"></i>
+                  </span>
+                  Email:
+                </h6>
+                <p className="mb-0">kusalcoc1212@gmail.com</p>
+              </ListGroupItem>
+              <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
+                <h6 className="mb-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-mail-line"></i>
+                  </span>
+                  Phone:
+                </h6>
+                <p className="mb-0">+94717428031</p>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+
+          <Col lg="12" className="text-center pt-5">
+            <p className="copyright">
+              Copyright {year}, design and develop by Kusal Punchihewa. All
+              rights reserved.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
