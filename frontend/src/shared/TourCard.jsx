3bc6732f41806de353553b8fe5fe03060fca0bbf
Kusal KRP <kusalcoc1212@gmail.com>
2023-12-23 00:10:19 +0530

    update

--- frontend/src/shared/TourCard.jsx
+++ frontend/src/shared/TourCard.jsx
@@ -29,7 +29,7 @@ const TourCard = ({ tour }) => {
             </span>
           </div>
           <h5 className="tour__title">
-            <Link to={"/tours/${id}"}>{title}</Link>
+            <Link to={`/tours/${id}`}>{title}</Link>
           </h5>
 
           <div className="card__bottom d-flex align-items-center justify-content-between mt-3">
@@ -37,7 +37,7 @@ const TourCard = ({ tour }) => {
               ${price} <span> /per person</span>
             </h5>
             <button className="btn booking__btn">
-              <Link to={"/tours/${id}"}>Book Now</Link>
+              <Link to={`/tours/${id}`}>Book Now</Link>
             </button>
           </div>
         </CardBody>
