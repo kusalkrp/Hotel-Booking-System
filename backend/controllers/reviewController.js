8dd1451f62ebfc50f0f11df9f67111582797c008
Kusal KRP <kusalcoc1212@gmail.com>
2023-12-24 20:52:58 +0530

    update

--- backend/controllers/reviewController.js
+++ backend/controllers/reviewController.js
@@ -1,4 +1,21 @@
+import Tour from "../models/Tour.js";
+import Review from "../models/Review.js";
+
 export const createReview = async (req, res) => {
+  const tourId = req.params.tourId;
+  const newReview = new Review({ ...req.body });
   try {
-  } catch {}
+    const savedReview = await newReview.save();
+
+    // after creating a new review now update the reviews array of the tour
+    await Tour.findByIdAndUpdate(tourId, {
+      $push: { review: savedReview._id },
+    });
+
+    res
+      .status(200)
+      .json({ succes: true, message: "Review submitted", data: savedReview });
+  } catch {
+    res.status(500).json({ succes: false, message: "failed to submit" });
+  }
 };
